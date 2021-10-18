936d884d445ff597398229c9793864e69680dc21
Priya Gupta <priyagupta9u@gmail.com>
2021-10-18 20:52:40 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const nav_trigger = document.querySelector(".nav-trigger");
-const site_wrapper = document.querySelector(".site-content-wrapper");
-
-nav_trigger.addEventListener("click", function () {
-  site_wrapper.classList.toggle("scaled");
-});
