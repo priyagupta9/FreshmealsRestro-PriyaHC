416103bf2035acb6e0cd6320e6dfc5da09bb6f96
Priya Gupta <priyagupta9u@gmail.com>
2021-11-08 15:32:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -132,7 +132,7 @@ function foot() {
 
 // -----------------countdown timer-----------------------
 const countdown = () =>{
-  const futureDate = new Date("October 30, 2021 00:00:00").getTime();
+  const futureDate = new Date("November 30, 2021 00:00:00").getTime();
   const currentDate = new Date().getTime();
   const gap = futureDate - currentDate;
 
